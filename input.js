51b6d6b9a58825e263513f2a58e6b5dc93e25cd4
Payam Beigi <beigi.payam@gmail.com>
2022-06-08 23:02:08 +0000

    Updated input to send a message

--- input.js
+++ input.js
@@ -6,19 +6,20 @@ const setupInput = (conn) => {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
   stdin.on("data", handleUserInput);
 
   return stdin;
 };
 
 const handleUserInput = function(key) {
+
   if (key === "\u0003") {
     process.exit();
   };
 
-  console.log("In in handleuserInput");
-  console.log(key);
   if (key === "w") {
+    connection.write("Move: up");
   };
 
   if (key === "a") {
@@ -31,6 +32,10 @@ const handleUserInput = function(key) {
 
   if (key === "s") {
     connection.write("Move: down");
+  };
+
+  if (key === "e") {
+    connection.write("Say: iMa EAT Yaaa!");
   };
 
 };
